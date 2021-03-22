032d53569385a7578831a81ef579edbbcccaf093
Ken Barlow <kenbrlow@gmail.com>
2021-03-21 22:27:29 -0600

    commit tree 8168a0a825253830bf2d0ec3ace8eddf41dfdfcb

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-// Your code here
+function mapToNegativize(arrayOfValues) {
+    return arrayOfValues.map(value => {
+        return value * -1;
+    })
+}
+
+function mapToNoChange(arrayOfValues) {
+    return arrayOfValues.map(value => {
+        return value;
+    })
+}
+
+function mapToDouble(arrayOfValues) {
+    return arrayOfValues.map(value => {
+        return value * 2;
+    })
+}
+
+function mapToSquare(arrayOfValues) {
+    return arrayOfValues.map(value => {
+        return value**2;
+    })
+}
+
+function reduceToTotal(arrayOfValues, startingPoint) {
+    let finalTotal = 0;
+    if (startingPoint != undefined) {
+      finalTotal += startingPoint;
+    }
+    finalTotal += arrayOfValues.reduce((a, b) => a + b, 0);
+    return finalTotal;
+}
+
+function reduceToAllTrue(arrayOfValues) {
+    let finalValue = false;
+    arrayOfValues.map(value => {
+        if (value) {
+            finalValue = true;
+        } else {
+            finalValue = false;
+        }
+    });
+}
+
+function reduceToAllTrue(arrayOfValues) {
+    return arrayOfValues.every(Boolean);
+}
+
+function reduceToAnyTrue(arrayOfValues) {
+    return arrayOfValues.reduce((a, b) => {
+        let finalValue = false;
+        if (a || b) {
+        finalValue = true;
+        }
+        return finalValue;
+    }, 0);
+}
